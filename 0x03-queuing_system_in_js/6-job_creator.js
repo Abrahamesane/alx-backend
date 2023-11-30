223443ef7f85e7c4bb8c6aef4dbae318be29b902
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-11-30 12:30:23 +0100

    Update 6-job_creator.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,16 +1,16 @@
 const kue = require('kue');
+const queue = kue.createQueue({name: 'push_notification_code'});
 
-const queue = kue.createQueue();
-
-const data = {
-  phoneNumber: '4153518780',
-  message: 'This is the code to verify your account',
+const JOB_DATA = {
+  phoneNumber: "0211",
+  message: "Your account has been created",
 };
 
-const queueName = 'push_notification_code';
-
-const job = queue.create(queueName, data).save();
-
-job.on('enqueue', () =>  console.log(`Notification job created: ${job.id}`))
-  .on('complete', () => console.log('Notification job completed'))
-  .on('failed', () => console.log('Notification job failed'));
+const push_notification_code = queue.create('push_notification_code', JOB_DATA).save( function(err) {
+  if (!err) {
+    console.log("Notification job created: " + push_notification_code.id);
+  } else {
+    console.log("Notification job failed");
+  }
+  push_notification_code.on('complete', () => console.log("Notification job completed"));
+});
