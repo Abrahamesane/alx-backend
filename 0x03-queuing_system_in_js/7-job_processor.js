4bf3f2d2c95ac8a3d970efd977c3db7e73528800
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-11-30 12:39:17 +0100

    Update 7-job_processor.js

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,23 +1,30 @@
-import kue from 'kue';
+import { createQueue, Job } from 'kue';
 
-const blacklisted = ['4153518780', '4153518781'];
+const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
+const queue = createQueue();
 
-const queue = kue.createQueue();
-const queueName = 'push_notification_code_2';
+const sendNotification = (phoneNumber, message, job, done) => {
+  let total = 2, pending = 2;
+  let sendInterval = setInterval(() => {
+    if (total - pending <= total / 2) {
+      job.progress(total - pending, total);
+    }
+    if (BLACKLISTED_NUMBERS.includes(phoneNumber)) {
+      done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+      clearInterval(sendInterval);
+      return;
+    }
+    if (total === pending) {
+      console.log(
+        `Sending notification to ${phoneNumber},`,
+        `with message: ${message}`,
+      );
+    }
+    --pending || done();
+    pending || clearInterval(sendInterval);
+  }, 1000);
+};
 
-function sendNotification(phoneNumber, message, job, done) {
-  const end_progress = 100;
-  
-  job.progress(0, end_progress);
-  
-  if (blacklisted.includes(phoneNumber)) {
-    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
-  }
-  job.progress(50, end_progress);
-  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-  done();
-}
-queue.process(queueName, 2, (job, done) => {
-  const { phoneNumber, message } = job.data;
-  sendNotification(phoneNumber, message, job, done);
+queue.process('push_notification_code_2', 2, (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
 });
