04aa690abd97332cc064c96bedee3fcef015ac5f
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-11-30 12:28:33 +0100

    Update 5-publisher.js

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -1,16 +1,19 @@
-import redis from 'redis';
+/*
+ * Publishes a message to the channel 'holberton school channel'
+ */
+import { createClient } from 'redis';
+const client = createClient();
 
-const client = redis.createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server: ' + err));
 
-client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+client.on('connection', () => console.log('Redis client connected to the server'));
 
-function publishMessage(message, time) {
+const publishMessage = (message, time) => {
   setTimeout(() => {
     console.log(`About to send ${message}`);
     client.publish('holberton school channel', message);
   }, time);
-}
+};
 
 publishMessage("Holberton Student #1 starts course", 100);
 publishMessage("Holberton Student #2 starts course", 200);
