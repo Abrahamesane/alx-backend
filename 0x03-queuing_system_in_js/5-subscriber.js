d4ba6bda217cfcfedfded3465172a7e82fc58ab8
abrahamesane <110992304+Abrahamesane@users.noreply.github.com>
2023-11-30 12:28:52 +0100

    Update 5-subscriber.js

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,16 +1,20 @@
-import redis from 'redis';
+/*
+ * Subscribes to holberton school channel
+ */
+import { createClient } from 'redis';
 
-const client = redis.createClient();
+const client = createClient();
 
+client.on("error", (err) => console.log('Redis client not connected to the server: ' + err));
 client.on('connect', () => console.log('Redis client connected to the server'));
-client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
 
 client.subscribe('holberton school channel');
 
-client.on('message', (channel, message) => {
-  if (channel === 'holberton school channel') console.log(message);
-  if (message === 'KILL_SERVER') {
-    client.unsubscribe();
-    client.quit();
+client.on('message', (err, msg) => {
+  console.log(msg);
+
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.end(true);
   }
 });
